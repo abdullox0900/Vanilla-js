01431e0b3aff29dde460df689c2eaadae02be564
abdullox0900 <abdullox0900@gmail.com>
2022-02-08 20:43:16 +0500

    feat: add new randon-img file

---
+++ 01 - random-img js/js/app.js
@@ -0,0 +1,56 @@
+let elBtn = document.querySelector(".btn");
+let elImg = document.querySelector(".cat-img");
+
+const apiUrl = "https://random.imagecdn.app/600/400";
+
+// ===================
+
+//OPTION N1
+// const getImgApi = async function () {
+//      const response = await fetch(apiUrl);
+
+//      elImg.src = response.url;
+
+//      console.log(response);
+// }
+
+// getImgApi()
+
+// elBtn.addEventListener("click", function () {
+//      getImgApi()
+// })
+
+// =====================
+
+//OPTION N2
+// async function getRandomImg() {
+//      const response = await fetch(apiUrl)
+
+//      elImg.src = response.url;
+// }
+// getRandomImg()
+
+// elBtn.addEventListener("click", function () {
+//      getRandomImg()
+// })
+
+// =====================
+
+//OPTION N3
+function getImgRandom(data) {
+     const response = fetch(apiUrl).then(res => res).then(data => getData(data));
+}
+
+function getData(data) {
+     elImg.src = data.url;
+}
+
+elBtn.addEventListener("click", function () {
+     getImgRandom()
+})
+
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
